a4d56fa0fbff317748bb63420ee2b33bc251d712
letrongdao <daoltse173161@fpt.edu.vn>
2024-06-09 22:54:55 +0700

    payos hash_signature[DaoLT]

--- src/modules/PayOS/payos.controller.js
+++ src/modules/PayOS/payos.controller.js
@@ -1,23 +1,24 @@
 const asyncHandler = require("../../utils/asynchandler");
 const { CREATED, OK } = require("../../core/success.response");
-const PayOSService = require('./payos.service')
+const PayOSService = require("./payos.service");
 
 class PayOSController {
-    createPaymentLink = asyncHandler(async (req, res) => {
-        const result = PayOSService.createPaymentLink(req.body)
-        new CREATED({
-            message: "Payment link created",
-            metadata: result,
-        }).send(res);
-    });
+  createPaymentLink = asyncHandler(async (req, res) => {
+    const result = PayOSService.createPaymentLink(req.body);
+    console.log("RESULT: ", result);
+    new CREATED({
+      message: "Payment link created",
+      metadata: result,
+    }).send(res);
+  });
 
-    getPaymentLinkInformation = asyncHandler(async (req, res) => {
-        const result = PayOSService.getPaymentLinkInformation(req.body.orderId)
-        new CREATED({
-            message: "Got payment link information",
-            metadata: result,
-        }).send(res);
-    });
+  getPaymentLinkInformation = asyncHandler(async (req, res) => {
+    const result = PayOSService.getPaymentLinkInformation(req.body.orderId);
+    new OK({
+      message: "Got payment link information",
+      metadata: result,
+    }).send(res);
+  });
 }
 
-module.exports = new PayOSController()
+module.exports = new PayOSController();
