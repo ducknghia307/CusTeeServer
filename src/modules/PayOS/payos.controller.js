5c3547f3ced5b538749ad3908c32da024f825280
ducknghia307 <ducnghia30703@gmail.com>
2024-06-09 22:53:59 +0700

    QR code

--- src/modules/PayOS/payos.controller.js
+++ src/modules/PayOS/payos.controller.js
@@ -4,11 +4,17 @@ const PayOSService = require('./payos.service')
 
 class PayOSController {
     createPaymentLink = asyncHandler(async (req, res) => {
-        const result = PayOSService.createPaymentLink(req.body)
-        new CREATED({
-            message: "Payment link created",
-            metadata: result,
-        }).send(res);
+        try {
+            const result = await PayOSService.createPaymentLink(req.body); // Await the promise here
+            console.log("::::::::::::",result);
+            new CREATED({
+                message: "Payment link created",
+                metadata: { checkoutUrl: result }, // Ensure the result is included in metadata
+            }).send(res);
+        } catch (error) {
+            console.error("Error in createPaymentLink handler: ", error);
+            res.status(500).json({ message: 'Internal Server Error' });
+        }
     });
 
     getPaymentLinkInformation = asyncHandler(async (req, res) => {
--- src/modules/PayOS/payos.service.js
+++ src/modules/PayOS/payos.service.js
@@ -4,22 +4,26 @@ const crypto = require('crypto')
 
 class PayOSService {
     static async createPaymentLink(data) {
-        // const checkSumKey = process.env.PAYOS_CHECKSUM_KEY
+        console.log('::::::::::::',data)
         const checkoutRequestData = {
             orderCode: data.orderCode,
             amount: data.amount,
             description: data.description,
-            cancelUrl: 'http://localhost:3000/cart',
-            returnUrl: 'http://localhost:3000/checkout/completed',
+            cancelUrl: data.cancelUrl || 'http://localhost:3000/cart', 
+            returnUrl: data.returnUrl || 'http://localhost:3000/checkout/completed',
             items: data.items,
             buyerName: data.buyerName,
             buyerPhone: data.buyerPhone,
-            expriredAt: data.expriredAt
-        }
-        const paymentLinkRes = await payOS.createPaymentLink(checkoutRequestData)
-        console.log("STATUS: ", paymentLinkRes.checkoutUrl);
-        return paymentLinkRes.checkoutUrl
+            expriredAt: data.expriredAt,
+        };
 
+        try {
+            const paymentLinkRes = await payOS.createPaymentLink(checkoutRequestData);
+            console.log("STATUS: ", paymentLinkRes.checkoutUrl);
+            return paymentLinkRes.checkoutUrl;
+        } catch (error) {
+            console.error("Error creating payment link: ", error);
+        }
     }
 
     static async getPaymentLinkInformation(orderId) {
