5b23d9c6e3d1b94d39b7bb927b281946af8b9da0
ducknghia307 <ducnghia30703@gmail.com>
2024-07-09 13:24:34 +0700

    fix cors[NghiaDD]

--- src/config/corsOptions.js
+++ src/config/corsOptions.js
@@ -9,6 +9,8 @@ const corsOptions = {
             callback(new Error('Not allowed by CORS'));
         }
     },
+    methods: ['GET', 'POST'],
+    allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
     optionsSuccessStatus: 200
 };
